22508afd11a843395f62d903224f38d1c341bbf9
billal <karimovbillal20002@gmail.com>
2023-01-10 04:05:52 +0600

    added async send new manga

---
+++ async_send_new_chapter.py
@@ -0,0 +1,27 @@
+from threading import Thread
+import time
+
+from loader import bot
+from vk_parser import get_parser_data
+
+with open('chat_ids.txt') as file:
+    chat_id = file.read()
+
+CHAT_ID = chat_id.split('\n')
+
+
+def send_manga_async(bot):
+    while True:
+        try:
+            for id_ in CHAT_ID:
+                parser_data = get_parser_data(id_)
+                if parser_data:
+                    for data in parser_data:
+                        bot.send_message(id_, data)
+            time.sleep(300)
+        except:
+            continue
+
+
+# Создаем новый поток и в нем запускаем нашу функцию:
+Thread(target=send_manga_async, args=(bot,)).start()
