0013a41977f0a54ee7bbe82be6b29456a56826b0
billal <karimovbillal20002@gmail.com>
2023-01-15 00:55:42 +0600

    changed menu

--- loader.py
+++ loader.py
@@ -4,12 +4,12 @@ from telebot import types
 
 bot = telebot.TeleBot(config('bot_token'))
 
-markup = types.InlineKeyboardMarkup()
-b1 = types.InlineKeyboardButton('Добавить мангу', callback_data='add_manga')
+markup = types.InlineKeyboardMarkup(row_width=2)
+b1 = types.InlineKeyboardButton('Доб мангу', callback_data='add_manga')
 b2 = types.InlineKeyboardButton('Список манг', callback_data='manga_list')
 b3 = types.InlineKeyboardButton('Удалить мангу', callback_data='del_manga')
-b4 = types.InlineKeyboardButton('Добавить группу', callback_data='add_group')
+b4 = types.InlineKeyboardButton('Доб группу', callback_data='add_group')
 b5 = types.InlineKeyboardButton('Список групп', callback_data='group_list')
 b6 = types.InlineKeyboardButton('Удалить группу', callback_data='del_group')
 b7 = types.InlineKeyboardButton('Отмена', callback_data='cancel')
-markup.add(b1, b2, b3, b4, b5, b6, b7)
+markup.add(b1, b4, b3, b6, b2, b5, b7)
